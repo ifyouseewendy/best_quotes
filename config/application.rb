690a78141a6a8f041a7252092bb6b9146d4ef857
wendi <ifyouseewendy@gmail.com>
2014-09-17 00:18:21 +0800

    Add Config.root and public index

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,21 @@
 require 'rulers'
 
+$LOAD_PATH << File.join( File.dirname(__FILE__), '..', 'app', 'controllers' )
+require 'quotes_controller'
+
 module BestQuotes
   class Application < Rulers::Application
+    def call(env)
+      env['ROOT_DIR'] = Config.root
+      super
+    end
+
+    class Config
+      class << self
+        def root
+          File.expand_path( File.join( File.dirname(__FILE__), '..') )
+        end
+      end
+    end
   end
 end
