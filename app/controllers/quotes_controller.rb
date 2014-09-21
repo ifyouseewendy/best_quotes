d09b5b31fe3207cc6ef3fe091b825b701097d6a4
wendi <ifyouseewendy@gmail.com>
2014-09-21 13:40:04 +0800

    Use request and params provided by Rulers

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,8 +1,13 @@
 class QuotesController < Rulers::Controller
   def index
-    p FileModel.all
     quotes = FileModel.all
     render :index, :quotes => quotes
+  end
+
+  def show
+    quote = FileModel.find(params['id'])
+    ua = request.user_agent
+    render_response :quote, :obj => quote, :ua => ua
   end
 
   def a_quote
